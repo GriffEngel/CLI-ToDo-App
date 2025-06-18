e4f82b6f648c63724ef853a373620b7ba0620f8d
Griffin Engel <gnengel07@gmail.com>
2025-06-17 20:35:38 -0500

    added tests

--- tests.py
+++ tests.py
@@ -3,7 +3,8 @@ from unittest.mock import patch, mock_open
 from datetime import datetime
 import json
 
-from main import load_tasks
+from main import load_tasks, add_task
+
 
 # TODO: Write tests
 class TestMyFunctions(unittest.TestCase):
@@ -11,27 +12,78 @@ class TestMyFunctions(unittest.TestCase):
         "builtins.open",
         new_callable=mock_open,
         read_data=json.dumps(
+            [
+                {
+                    "id": 1,
+                    "Description": "Do the dishes",
+                    "status": "todo",
+                    "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                }
+            ]
+        ),
+    )
+    def test_load_tasks(self, mock_open):
+        filename = "tasks.json"
+        # call function that is being tested
+        actual_output = load_tasks(filename)
+        # make sure it's being opened with correct arguments
+        mock_open.assert_called_with(filename, "r")
+        expected_output = [
             {
                 "id": 1,
-                "Description": "Do this dishes",
+                "Description": "Do the dishes",
                 "status": "todo",
                 "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             }
-        ),
-    )
-    def test_load_tasks(self, filename):
-        expected_output = {
-            "id": 1,
-            "Description": "Do this dishes",
+        ]
+
+        self.assertEqual(expected_output, actual_output)
+
+    # ! Fix Test
+    def test_add_task():
+        task_data = {
+            "id": 2,
+            "Description": "Do the dishes",
             "status": "todo",
             "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         }
+        add_task()
+        mock_open.write.assert_called_once_with(
+            json.dumps(
+                [
+                    {
+                        "id": 1,
+                        "Description": "Do the dishes",
+                        "status": "todo",
+                        "created_at": datetime.now().strftime(
+                            "%Y-%m-%d %H:%M:%S"
+                        ),
+                        "updated_at": datetime.now().strftime(
+                            "%Y-%m-%d %H:%M:%S"
+                        ),
+                    },
+                    task_data,
+                ]
+            )
+        )
 
-        filename = "tasks.json"
-        actual_output = load_tasks(filename)
+        expected_output = [
+            {
+                "id": 2,
+                "Description": "Do the dishes",
+                "status": "todo",
+                "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            }
+        ]
 
-        mock_file.assert_called_with(filename)
+        actual_output = load_tasks("tasks.json")
 
         self.assertEqual(expected_output, actual_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
